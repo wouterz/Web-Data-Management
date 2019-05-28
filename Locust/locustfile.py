75831976b7ed3368a075058337e037fa5c2fab1f
Chris Mostert <c.mostert@student.tudelft.nl>
2019-05-28 14:20:18 +0200

    Make client side order an object for easier user task behaviour

--- Locust/locustfile.py
+++ Locust/locustfile.py
@@ -1,61 +1,44 @@
 from locust import HttpLocust, TaskSet, task
 import random
 
+class Order:
+    ''' Constructor '''
+    def __init__(self, userid, orderid):
+        self.userid = userid
+        self.orderid = orderid
+        self.items = []
+        self.paid = False
+
+    def addItem(itemid):
+        self.items.append(itemid)
+
+    def setPaid(bool):
+        self.paid = bool
+
 class UserBehavior(TaskSet):
-    userid = None
-    orderid = None
-    items = []
 
     def on_start(self):
         """ This function is called before any of the defined tasks is scheduled
         It should create a new user with a random amount of credit
-        and save the ID for future requests
-        Should be updated to reflect how the ID is returned! """
-        response = self.client.post("/users/create")
-        self.userid = response.text
+        and save the userid for future requests """
+
+        userResponse = self.client.post("/users/create")
+        self.userid = userResponse.text
         self.client.post("/users/credit/add", {"user_id": self.userid, "amount": random.randint(1,1000)})
 
-        # Make sure that we have an order
+        # Create a list of orders for this user
+        self.orders = []
+
+        # Create an initial order
         response = self.client.post("/orders/create/" + self.userid)
-        self.order = response.text
+        self.orders.append(self.userid, response.text)
 
         # Make sure that at least one item exists
         response = self.client.post("/stock/item/create")
-        self.items.append(response.text)
+        self.orders[0].addItem(response.text)
 
     def on_stop(self):
         """ This function is called when the taskset is stopping """
         self.client.delete("/users/remove/" + self.userid)
-
-    def randomItemIndex():
-        return random.randint(0, len(self.items)-1)
-
-
-    """ List of possible tasks """
-
-    """ User service """
-    @task
-    def finduser(self):
-        self.client.get("/users/find/" + self.userid)
-
-    @task
-    def findcredit(self):
-        self.client.get("/users/credit/" + self.userid)
-
-    """ Order service """
-    @task
-    def createneworder(self):
-        self.client.delete("/orders/remove/" + self.orderid)
-        response = self.client.post("/orders/create/" + self.userid)
-        self.orderid = response.text  
-        
-    @task
-    def findorder(self):
-        self.client.get("/orders/find/" + self.orderid)
-
-    @task
-    def additem(self):
-        index = randomItemIndex()
-        self.client.post("/orders/addItem", {"order_id": self.orderid, "item_id": self.items[index]})
 
     
