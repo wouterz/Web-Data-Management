70d6052b764be08cabc33ea1f4d584cd41a11a56
Michiel van den Berg <M.R.vandenBerg-3@student.tudelft.nl>
2019-06-04 15:47:09 +0200

    Add redisconfig files + localrepo's for stock and order

--- stock-service/src/main/java/service/stock/storage/LocalRepository.java
+++ stock-service/src/main/java/service/stock/storage/LocalRepository.java
@@ -1,40 +1,46 @@
 package service.stock.storage;
 
 import org.springframework.stereotype.Repository;
+
+import service.stock.models.Order;
 import service.stock.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class LocalRepository implements Dao {
+	
+	private final AtomicLong counter = new AtomicLong();
 
-    private List<User> Users = new ArrayList<>();
+    private List<Order> Orders = new ArrayList<>();
 
     @Override
-    public long create(long id) {
-        User user = new User(id, 0);
-        Users.add(user);
-        return id;
+    public long create(long userId) {
+    	long orderId = counter.getAndIncrement();
+        Order order = new Order(userId, orderId);
+        Orders.add(order);
+        return orderId;
     }
 
     @Override
-    public Object update(long id, Object user) {
-        Users.set((int)id, (User) user);
-        return user;
+    public Object update(long id, Object order) {
+        Orders.set((int)id, (Order) order);
+        return order;
     }
 
     @Override
-    public User get(long id) {
+    public Order get(long id) {
         List<Long> ids = new ArrayList<>(1);
         ids.add(id);
-        return Users.stream().filter(p -> ids.contains(p.getId())).findFirst().orElse(null);
+        return Orders.stream().filter(p -> ids.contains(p.getorderId())).findFirst().orElse(null);
     }
 
     @Override
     public boolean delete(long id) {
-        User user = Users.remove((int) id);
-        return user.getId() != 0;
+    	Order order = Orders.remove((int) id);
+        return order.getorderId() != 0;
     }
 
 
