7197326e2ea3e59a2e59b43efd25d8919d782d63
daanwerf <daanvanderwerf@hotmail.com>
2019-06-07 17:33:32 +0200

    added connect method

--- stock-service/src/main/java/service/stock/storage/PostgresRepository.java
+++ stock-service/src/main/java/service/stock/storage/PostgresRepository.java
@@ -2,10 +2,33 @@ package service.stock.storage;
 
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @Repository
 public class PostgresRepository implements Dao {
 
+    /**
+     * Create a connection to the AWS Postgres database
+     *
+     * @return Connection to the database
+     */
+    private static Connection connectoRDS() {
+        Connection c = null;
+        try {
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://webdata.cbcu76qz5fg7.us-east-1.rds.amazonaws.com:5432/webdata",
+                            "webdata", "reverse123");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Connected to database successfully");
 
+        return c;
+    }
+    
     @Override
     public String create(long id) {
         return "0";
