649d927fec34135bb17ce3c700a43c808d0b605a
daanwerf <daanvanderwerf@hotmail.com>
2019-06-07 17:29:55 +0200

    changed stockItem class to incorporate new IDs

--- stock-service/src/main/java/service/stock/models/StockItem.java
+++ stock-service/src/main/java/service/stock/models/StockItem.java
@@ -4,31 +4,47 @@ import java.io.Serializable;
 import java.util.UUID;
 
 public class StockItem implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private final String id;
     private final String name;
     private int stock;
 
-    public StockItem(long id, String name){
+    /**
+     * Constructor for existing StockItems retrieved from the database
+     *
+     * @param id   Id of the stockItem
+     * @param name Name of the stockItem
+     */
+    public StockItem(String id, String name, int stock) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    /**
+     * Constructor for a new StockItem
+     * @param name Name of the StockItem
+     */
+    public StockItem(String name) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.stock = 0;
     }
 
-    public void addToStock(int amount){
+    public void addToStock(int amount) {
         this.stock += amount;
     }
 
-    public String getId(){
+    public String getId() {
         return this.id;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getStock(){
+    public int getStock() {
         return this.stock;
     }
 
