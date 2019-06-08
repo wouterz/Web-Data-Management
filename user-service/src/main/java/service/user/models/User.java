9823298ee85882b5d184ab07ed9aece3718e39cf
daanwerf <daanvanderwerf@hotmail.com>
2019-06-09 00:19:09 +0200

    update all models for user-service

--- user-service/src/main/java/service/user/models/User.java
+++ user-service/src/main/java/service/user/models/User.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
 
     private final String id;
