b0391e2b068c5b636227d1f85adb2af3162c57ae
Wouter Zirkzee <W.J.Zirkzee@student.tudelft.nl>
2019-06-09 20:01:18 +0200

    more rmi to requestmapping

--- payment-service/src/main/java/service/payment/RMI/UserClient.java
+++ payment-service/src/main/java/service/payment/RMI/UserClient.java
@@ -8,10 +8,10 @@ import service.payment.models.StockItem;
 @FeignClient("user-service")
 public interface UserClient {
 
-    @PostMapping("/user/{user_id}/credit/add/{amount}")
+    @RequestMapping("/user/{user_id}/credit/add/{amount}")
     Boolean addCredits(@PathVariable(value = "user_id") String user_id, @PathVariable(value = "amount") long amount);
 
-    @PostMapping("/user/{user_id}/credit/subtract/{amount}")
+    @RequestMapping("/user/{user_id}/credit/subtract/{amount}")
     Boolean subtractCredits(@PathVariable(value = "user_id") String user_id, @PathVariable(value = "amount") long amount);
 
 }
