483b9d695bd4facf37340b628839f1b286cb1b89
daanwerf <daanvanderwerf@hotmail.com>
2019-06-09 14:29:11 +0200

    add missing redis methods and update for new DAO

--- payment-service/src/main/java/service/payment/storage/RedisRepository.java
+++ payment-service/src/main/java/service/payment/storage/RedisRepository.java
@@ -1,28 +1,46 @@
 package service.payment.storage;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.stereotype.Repository;
+
+import service.payment.models.Order;
 import service.payment.models.Payment;
+import service.payment.models.Payment.PaymentStatus;
 
 @Repository
 public class RedisRepository implements Dao {
 
+    private static final String KEY = "payments";
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, Payment> hashOps;
+
+
     @Override
     public Payment create(Object o) {
-        return null;
+        Payment payment = (Payment) o;
+        hashOps.putIfAbsent(KEY, payment.getId(), payment);
+        return payment;
     }
 
     @Override
-    public Payment get(String id) {
-        return null;
+    public Object get(String id) {
+        Payment payment = hashOps.get(KEY, id);
+        return payment;
     }
 
     @Override
-    public Payment update(Object o) {
-        return null;
+    public Object update(Object o) {
+        Payment payment = (Payment) o;
+        hashOps.put(KEY, payment.getId(), payment);
+        return payment;
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        hashOps.delete(KEY, id);
+        return hashOps.get(KEY, id) == null;
     }
 }
